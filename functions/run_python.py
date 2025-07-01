8d8325c5bd00c99d255264b5e36ea08434b46bbd
adhamamr01 <adhamamr01.aa@gmail.com>
2025-07-01 20:47:17 +0300

    Added the last (scary) function

---
+++ functions/run_python.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+def run_python_file(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory,file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith('.py'):
+        return f'Error: File "{file_path}" is not a Python file.'
+    try:
+        result = subprocess.run(
+            ["uv", "run", abs_file_path],
+            timeout=30,
+            cwd=abs_working_dir,
+            capture_output=True,
+            text=True
+        )
+        output = ''
+        if result.stdout:
+            output += f"STDOUT: {result.stdout}"
+        if result.stderr:
+            output += f"STDERR: {result.stderr}"
+        if result.returncode != 0:
+            output += f"Process exited with code {result.returncode}\n"
+        if not output.strip():
+            output = "No output produced."
+        return output
+    except Exception as e:
+        return f"Error during execution: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,15 +1,18 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 print(f"Result for first test:")
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-)
+print(run_python_file("calculator", "main.py"))
+
 
 print(f"Result for second text:")
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-)
+print(run_python_file("calculator", "tests.py"))
+
 
 print(f"Result for third test:")
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+print(run_python_file("calculator", "../main.py"))
 
+print(f"Result for fourth test:")
+print(run_python_file("calculator", "nonexistent.py"))
